d0420d3fa1267f0ff63088ad0becfa1be8516075
221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-04-17 15:33:08 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,30 +1,33 @@
 # python3
 
 def read_input():
-   choice = input()
-if "I" in choice[:1]:
-    text = input().rstrip()
-    pattern = input().rstrip()
-    
- elif "F" in choice:
-     with open("./tests/06", "r") as file:
-          text = file.readline().rstrip()
-          pattern = file.readline().rstrip()
-            
+    choice = input()
+    if "I" in choice[:1]:
+        text = input().rstrip()
+        pattern = input().rstrip()
+    elif "F" in choice:
+        with open("./tests/06", "r") as file:
+            text = file.readline().rstrip()
+            pattern = file.readline().rstrip()
     return (text, pattern)
 
 def print_occurrences(output):
-    
     print(' '.join(map(str, output)))
+
 def get_occurrences(pattern, text):
-     occurances = []
-     pattern_len = len(pattern)
-     text_len = len(text)
-     for i in range(text_len - pattern_len + 1):
-         text1 = text[i:i + pattern_len]
-         if hash(text1) == hash(pattern):
-            occurances.append(i)
-            return occurances
+    occurrences = []
+    pattern_len = len(pattern)
+    text_len = len(text)
+    pattern_hash = hash(pattern)
+    for i in range(text_len - pattern_len + 1):
+        text1 = text[i:i + pattern_len]
+        if hash(text1) == pattern_hash:
+            if text1 == pattern:
+                occurrences.append(i)
+    return occurrences
+
+
+
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow 
     # after input type choice
